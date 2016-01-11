279da450fd99c4d1f8ddef8cf8888f59f3ae0f64
Tor Bair <torbair@gmail.com>
2016-01-11 08:59:21 -0800

    lol

--- sloanbot.js
+++ sloanbot.js
@@ -43,7 +43,7 @@ var controller = Botkit.slackbot({
 	debug: false,
 });
 
-controller.setupWebserver(process.env.port,function(err,webserver) {
+controller.setupWebserver(3000,function(err,webserver) {
 
  controller.createWebhookEndpoints(controller.webserver);
 
